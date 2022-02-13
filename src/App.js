dfa74931ee3b1fb500d30880f7708412cefb1ca4
Swagat Das <swagat.das@navi.com>
2022-02-14 00:26:39 +0530

    special commit

--- src/App.js
+++ src/App.js
@@ -5,9 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Fblack%2F&psig=AOvVaw1BnScJ1zE_vOJS2iOLZewX&ust=1644864470272000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMiG1-Wr_fUCFQAAAAAdAAAAABAJ" className="App-logo" alt="logo" />
         <p>
+<<<<<<< Updated upstream
           Bye there!
+=======
+          Swati, the prettiest lady.
+>>>>>>> Stashed changes
         </p>
         <a
           className="App-link"
