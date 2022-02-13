6823d1e11118e134e21f96a787f4ce7615a69b8e
Swagat Das <swagat.das@navi.com>
2022-02-14 02:46:12 +0530

    Revert "mew mew"
    This reverts commit 478d2616d5b3d006a0123e9d71f028fe2dbe3291.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,20 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App" style="background-image: url(https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fphotoshop&psig=AOvVaw0VREvOpaTG01NWoeBdZDoN&ust=1644865782499000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCIj5ltiw_fUCFQAAAAAdAAAAABAD)">
+    <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Swati, the prettiest lady!
+          Bye there!
         </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
